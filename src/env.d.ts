11a9c8d42462d37fcbbbaa7bf37070fd0634b7cf
Riley1101 <www.riley.s.eileen@gmail.com>
2023-01-31 17:38:43 -0500

    Initial commit with layouts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly PUBLIC_SANITY_PROJECT_ID: string;
+  readonly PUBLIC_SANITY_DATASET: string;
+  readonly PUBLIC_SANITY_API_VERSION: string;
+  readonly SANITY_TOKEN: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- src/utils/sanityClient.ts
+++ src/utils/sanityClient.ts
@@ -1,11 +1,11 @@
 import sanityClient from "@sanity/client";
 
 const client = sanityClient({
-  projectId: "your-project-id",
-  dataset: "bikeshop",
-  apiVersion: "2021-03-25", // use current UTC date - see "specifying API version"!
-  token: "sanity-auth-token", // or leave blank for unauthenticated usage
-  useCdn: true, // `false` if you want to ensure fresh data
+  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID as string,
+  dataset: import.meta.env.PUBLIC_SANITY_DATASET as string,
+  apiVersion: import.meta.env.PUBLIC_SANITY_API_VERSION as string, // use current UTC date - see "specifying API version"!
+  token: import.meta.env.SANITY_TOKEN as string, // or leave blank for unauthenticated usage
+  useCdn: false, // `false` if you want to ensure fresh data
 });
 
 export default client;
